021e1d1102909bf40a89cdd99912a6fdad1622ea
Michael McClain <Mikemcclain14@gmail.com>
2022-10-18 11:25:36 +0200

    Video 3.38 finished

--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Rigidbody rb;
+    [SerializeField] float mainThrust = 1000f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +24,7 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddRelativeForce(Vector3.up);
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
         }
     }
 
