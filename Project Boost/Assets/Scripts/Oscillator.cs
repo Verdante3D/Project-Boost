4b2e5b87e7ce3688351c46365013a356e0da94b0
Verdante3D <Mikemcclain14@gmail.com>
2022-10-22 12:49:09 +0200

    Video 3.57 finished

---
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPostion;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] [Range(0,1)] float movementFactor;
+
+    void Start()
+    {
+        startingPostion = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPostion + offset;
+    }
+}
